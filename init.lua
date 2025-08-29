9ee821acc36d97067dd2ee2e5e7b09a4ca297c7e
John Mendonca <joaosinho@gmail.com>
2025-08-28 21:53:45 -0600

    Recognize various .env files

--- init.lua
+++ init.lua
@@ -166,6 +166,14 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   end,
 })
 
+-- Detect other .env files
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = { '.env.*' },
+  callback = function()
+    vim.bo.filetype = 'sh'
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
